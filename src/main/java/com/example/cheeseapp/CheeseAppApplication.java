15ae04053332e9e514a5cade9ffb75e54f0ca615
Michalina Maria <michalina.dev@gmail.com>
2023-09-03 13:04:55 +0200

    Initial commit

---
+++ src/main/java/com/example/cheeseapp/CheeseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheeseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cheeseapp/CheeseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheeseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
